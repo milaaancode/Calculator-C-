7949c6fed7b7e36047b3ff0c733af9754029d6a8
milaaancode <90990880+milaaancode@users.noreply.github.com>
2021-09-19 01:21:49 +0200

    Math Quiz

---
+++ Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+    public partial class form1 : Form
+    {
+        // Креирање случајног објекта званог рандомизер
+        // за генерисање случајних бројева.
+        Random randomizer = new Random();
+
+        int addend1;
+        int addend2;
+
+        int timeLeft;
+
+        int minuend;
+        int substrhend;
+
+        int multiplier;
+        int multiplicand;
+
+        int divident;
+        int divisor;
+
+        public form1()
+        {
+            InitializeComponent();
+        }
+
+        public void StartTheQuiz()
+        {
+            addend1 = randomizer.Next(51);
+            addend2 = randomizer.Next(51);
+            plusLeftLabel.Text = addend1.ToString();
+            plusRightLabel.Text = addend2.ToString();
+            sum.Value = 0;
+
+            minuend = randomizer.Next(1, 101);
+            substrhend = randomizer.Next(1, minuend);
+            minusLeftLabel.Text = minuend.ToString();
+            minusRightLabel.Text = substrhend.ToString();
+            difference.Value = 0;
+
+            multiplicand = randomizer.Next(2, 11);
+            multiplier = randomizer.Next(2, 11);
+            timesLeftLabel.Text = multiplicand.ToString();
+            timesRightLabel.Text = multiplier.ToString();
+            product.Value = 0;
+
+            divisor = randomizer.Next(2, 11);
+            int temporaryQuotient = randomizer.Next(2, 11);
+            divident = divisor * temporaryQuotient;
+            devideLeftLabel.Text = divident.ToString();
+            devidedRightLabel.Text = divisor.ToString();
+            quotient.Value = 0;
+
+            timeLeft = 30;
+            timeLabel.Text = "30 seconds";
+            timer1.Start();
+        }
+
+        private bool CheckTheAnswer()
+        {
+            if ((addend1 + addend2 == sum.Value) && 
+                (minuend - substrhend == difference.Value)&&
+                (multiplicand*multiplier==product.Value)&&
+                (divident/divisor==quotient.Value))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            StartTheQuiz();
+            startButton.Enabled = false;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (CheckTheAnswer())
+            {
+                timer1.Stop();
+                MessageBox.Show("You got all the answers right!", "Congratulations!");
+                startButton.Enabled = true;
+            }
+            else if (timeLeft > 0)
+            {
+                timeLeft--;
+                timeLabel.Text = timeLeft + " seconds";
+            }
+            else
+            {
+                timer1.Stop();
+                timeLabel.Text = "Time is up!";
+                MessageBox.Show("You didn't finish in time.", "Sorry!");
+                sum.Value = addend1 + addend2;
+                difference.Value = minuend - substrhend;
+                product.Value = multiplicand * multiplier;
+                quotient.Value = divident / divisor;
+                startButton.Enabled = true;
+            }
+        }
+
+        private void answer_Enter(object sender, EventArgs e)
+        {
+            NumericUpDown answerBox = sender as NumericUpDown;
+            if (answerBox != null)
+            {
+                int lengthOfAnswer = answerBox.Value.ToString().Length;
+                answerBox.Select(0, lengthOfAnswer);
+            }
+        }
+
+        private void timeLabel_Click(object sender, EventArgs e)
+        {
+            if (timeLeft <= 5)
+            {
+                timeLabel.ForeColor = Color.Red;
+                
+            }
+        }
+
+        private void endButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
